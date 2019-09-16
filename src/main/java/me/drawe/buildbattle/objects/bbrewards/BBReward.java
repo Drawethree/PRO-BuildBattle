439c9fe1e6cdb0174645e1eb681eba632910ad66
Drawethree <janci.kluka@gmail.com>
2019-09-16 23:57:00 +0200

    Removed

--- src/main/java/me/drawe/buildbattle/objects/bbrewards/BBReward.java
+++
@@ -1,8 +0,0 @@
-package me.drawe.buildbattle.objects.bbrewards;
-
-import me.drawe.buildbattle.objects.bbobjects.BBTeam;
-
-public interface BBReward {
-
-    void giveReward(BBTeam team, int placement);
-}
