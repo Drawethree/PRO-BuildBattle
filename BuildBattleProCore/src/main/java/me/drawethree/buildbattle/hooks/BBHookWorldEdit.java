3ddfc1c28f265c80d6215b9177020fe9e168eeda
Drawethree <janci.kluka@gmail.com>
2020-04-19 16:07:04 +0200

    Overriden hook method from parent

--- BuildBattleProCore/src/main/java/me/drawethree/buildbattle/hooks/BBHookWorldEdit.java
+++ BuildBattleProCore/src/main/java/me/drawethree/buildbattle/hooks/BBHookWorldEdit.java
@@ -3,6 +3,7 @@ package me.drawethree.buildbattle.hooks;
 import lombok.Getter;
 import me.drawethree.buildbattle.BuildBattle;
 import me.drawethree.buildbattle.managers.ReportManager;
+import org.bukkit.Bukkit;
 
 public class BBHookWorldEdit extends BBHook {
 
@@ -19,5 +20,18 @@ public class BBHookWorldEdit extends BBHook {
         this.reportManager.loadAllReports();
     }
 
+    @Override
+    public void hook(BuildBattle buildBattle) {
+        if (Bukkit.getPluginManager().isPluginEnabled(pluginName)) {
+            String version = Bukkit.getPluginManager().getPlugin(pluginName).getDescription().getVersion();
 
+            if (version.startsWith("7")) {
+                BuildBattle.getInstance().info("§aSuccessfully hooked into §e" + this.pluginName + " §aversion §e" + version);
+                this.enabled = true;
+                this.runHookAction(buildBattle);
+            } else {
+                BuildBattle.getInstance().warning("§cThis version of WorldGuard is not supported!(§e" + version + "§c). Please use version §e7.0.0 §cand above.");
+            }
+        }
+    }
 }
